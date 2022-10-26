126e7ff44b568c4fe6cb7ba79e864ef6547e279f
cesarKadir <torrico.villanueva.cesar.kadir@gmail.com>
2022-10-26 01:15:25 +0000

    Creating the dataset for trainig

--- trainig.py
+++ trainig.py
@@ -6,3 +6,20 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import random
+
+data_train = pd.read_csv('daily-train.csv')
+data_validation = pd.read_csv('daily-validation.csv')
+data_test = pd.read_csv('daily-test.csv')
+data_train['dialog']
+def get_row_len(row):
+    return len(row['dialog'])
+data_train['len_dialog'] = data_train.apply(get_row_len, axis = 1)
+data_train = data_train[data_train['len_dialog']>1020]
+
+dd = LSTM_Dataset('daily-train.csv')
+dl = DataLoader(dd, batch_size = 16, shuffle = False)
+for example in dl:
+  #print(example['x'].shape)
+  print(example['x'].shape, example['y'].shape)
+
+  break
